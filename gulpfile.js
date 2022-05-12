6972219120730d29838a50792c197674172ebb81
MrBrims <magistrbrims@gmail.com>
2022-05-12 18:16:05 +0800

    added hero, describe and specs section

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,8 @@ function styles() {
 function scripts() {
   return src([ 
     'node_modules/rellax/rellax.min.js',
+    'node_modules/swiper/swiper-bundle.min.js',
+    'node_modules/modal-video/js/modal-video.min.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
